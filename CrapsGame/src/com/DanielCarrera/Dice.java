d55a16f1e40dbbb6f1e7a050bb6c513d66e3f0d1
daniel-carrera <dcarre1@ucsc.edu>
2020-12-19 23:33:58 -0800

    first commit, just started planning out craps game, files are bare bones as of now

---
+++ CrapsGame/src/com/DanielCarrera/Dice.java
@@ -0,0 +1,4 @@
+package com.DanielCarrera;
+
+public class Dice {
+}
---
+++ CrapsGame/src/com/DanielCarrera/Main.java
@@ -0,0 +1,8 @@
+package com.DanielCarrera;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
